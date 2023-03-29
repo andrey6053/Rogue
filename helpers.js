e9a52247bddac31b4be1c1a1ecff3a0cf5f4e553
andrey6053 <andreynick95@gmail.com>
2023-03-29 23:15:32 +0300

    prettier

--- helpers.js
+++ helpers.js
@@ -1,137 +1,143 @@
 function randomNum(max, min, array) {
-    if (!array) return Math.floor(Math.random() * (max - min) + min)
-    const filteredArray = []
-    while (filteredArray.length < randomNum(6, 3)) {
-       let randNum = Math.floor(Math.random() * (max - min) + min)
-       const rightNum = randNum + 1
-       const leftNum = randNum - 1
-       if (filteredArray.indexOf(randNum) === -1 && filteredArray.indexOf(rightNum) === -1 && filteredArray.indexOf(leftNum) === -1) {
-          filteredArray.push(randNum)
-       }
+  if (!array) return Math.floor(Math.random() * (max - min) + min);
+  const filteredArray = [];
+  while (filteredArray.length < randomNum(6, 3)) {
+    let randNum = Math.floor(Math.random() * (max - min) + min);
+    const rightNum = randNum + 1;
+    const leftNum = randNum - 1;
+    if (
+      filteredArray.indexOf(randNum) === -1 &&
+      filteredArray.indexOf(rightNum) === -1 &&
+      filteredArray.indexOf(leftNum) === -1
+    ) {
+      filteredArray.push(randNum);
     }
-    filteredArray.map(el => array.push(el))
-    return array
+  }
+  filteredArray.map((el) => array.push(el));
+  return array;
 }
 
 function gameOver(title) {
-    const titlePopup = document.querySelector(".popup__title")
-    game.popup.style.display = "flex"
-    titlePopup.textContent = title
+  const titlePopup = document.querySelector(".popup__title");
+  game.popup.style.display = "flex";
+  titlePopup.textContent = title;
 }
 
 function kill(index) {
-    game.gameMap[index][2] = "tile"
-    game.gameMap[index].pop
-    render()
+  game.gameMap[index][2] = "tile";
+  game.gameMap[index].pop;
+  render();
 }
 
 function moveValidate(col, row, posHero, ind, type) {
-    let index
-    const posToMove = game.gameMap.filter((el, i) => {
-       if (el[0] === posHero[0] + col && el[1] === posHero[1] + row) {
-          index = i
-          return true
-       }
-    })
-    if (posToMove.length === 0) return
-    const pos = posToMove[0][2]
-    if (type === "enemy") {
-       if (pos !== "tile") return
-       game.gameMap[ind][2] = "tile"
-       game.gameMap[index][2] = type
-       const dataSet = game.gameMap[ind].pop()
-       game.gameMap[index].push(dataSet)
-       render()
-       return true
+  let index;
+  const posToMove = game.gameMap.filter((el, i) => {
+    if (el[0] === posHero[0] + col && el[1] === posHero[1] + row) {
+      index = i;
+      return true;
     }
-    if (pos === "wall" || pos=== "enemy") return
-    if (pos === "sword") {
-       game["heroPower"] += 20
-    }
-    if (pos === "potion") {
-       game["heroHealth"] + 20 > 100 ? game["heroHealth"] = 100 : game["heroHealth"] += 20
-    }
-    game.gameMap[ind][2] = "tile"
-    game.gameMap[index][2] = type
-    render()
-    return true
- }
+  });
+  if (posToMove.length === 0) return;
+  const pos = posToMove[0][2];
+  if (type === "enemy") {
+    if (pos !== "tile") return;
+    game.gameMap[ind][2] = "tile";
+    game.gameMap[index][2] = type;
+    const dataSet = game.gameMap[ind].pop();
+    game.gameMap[index].push(dataSet);
+    render();
+    return true;
+  }
+  if (pos === "wall" || pos === "enemy") return;
+  if (pos === "sword") {
+    game["heroPower"] += 20;
+  }
+  if (pos === "potion") {
+    game["heroHealth"] + 20 > 100
+      ? (game["heroHealth"] = 100)
+      : (game["heroHealth"] += 20);
+  }
+  game.gameMap[ind][2] = "tile";
+  game.gameMap[index][2] = type;
+  render();
+  return true;
+}
 
- function createRooms() {
-    const cols = []
-    const rooms = []
-    const height = randomNum(7, 4)
-    const width = randomNum(7, 4)
-    for (let i = 0; i < randomNum(6, 3); i++) {
-       let randomCol = randomNum(game.columns-5, 0)
-       let randomRow = randomNum(game.rows-2, 0)
-       for (let i = 1; i < height; i++) {
-          cols.push({ col: randomCol += 1, row: randomRow })
-       }
+function createRooms() {
+  const cols = [];
+  const rooms = [];
+  const height = randomNum(7, 4);
+  const width = randomNum(7, 4);
+  for (let i = 0; i < randomNum(6, 3); i++) {
+    let randomCol = randomNum(game.columns - 5, 0);
+    let randomRow = randomNum(game.rows - 2, 0);
+    for (let i = 1; i < height; i++) {
+      cols.push({ col: (randomCol += 1), row: randomRow });
     }
-    cols.map(el => {
-       for (let i = 1; i < width; i++) {
-          rooms.push({ col: el.col, row: el.row += 1 })
-       }
-    })
-    rooms.map(el => {
-       game.gameMap.map(elem => {
-          if (elem[0] === el.col && elem[1] === el.row) {
-             elem[2] = "tile"
-          }
-       })
-    })
- }
+  }
+  cols.map((el) => {
+    for (let i = 1; i < width; i++) {
+      rooms.push({ col: el.col, row: (el.row += 1) });
+    }
+  });
+  rooms.map((el) => {
+    game.gameMap.map((elem) => {
+      if (elem[0] === el.col && elem[1] === el.row) {
+        elem[2] = "tile";
+      }
+    });
+  });
+}
 
- function createRoads() {
-    const randomCol = randomNum(game.columns, 0, [])
-    const randomRow = randomNum(game.rows, 0, [])
-    randomCol.map(el => {
-       game.gameMap.map(elTile => {
-          if (elTile[0] === el) {
-             elTile[2] = "tile"
-          }
-       })
-    })
-    randomRow.map(el => {
-       game.gameMap.map(elTile => {
-          if (elTile[1] === el) {
-             elTile[2] = "tile"
-          }
-       })
-    })
- }
-
+function createRoads() {
+  const randomCol = randomNum(game.columns, 0, []);
+  const randomRow = randomNum(game.rows, 0, []);
+  randomCol.map((el) => {
+    game.gameMap.map((elTile) => {
+      if (elTile[0] === el) {
+        elTile[2] = "tile";
+      }
+    });
+  });
+  randomRow.map((el) => {
+    game.gameMap.map((elTile) => {
+      if (elTile[1] === el) {
+        elTile[2] = "tile";
+      }
+    });
+  });
+}
 
 function fightValidate(vertical, gorizont, posHero, type) {
-    let index
-    const posToAttack = game.gameMap.filter((el, i) => {
-       if (el[0] === posHero[0] + vertical && el[1] === posHero[1] + gorizont) {
-          index = i
-          return true
-       }
-    })
-    if (posToAttack.length === 0) return
-    const pos = posToAttack[0][2]
-    if (type === "hero" && pos === "enemy") {
-       const dataSet = posToAttack[0][3]
-       const tagEnemy = document.querySelector(`[data-enemy = '${dataSet}']`)
-       game[`${dataSet}-enemyHealth`] -= game.heroPower
-       if (game[`${dataSet}-enemyHealth`] <= 0) {
-          kill(index)
-          if (document.querySelectorAll(".tileE").length === 0) gameOver("You Won!")
-          return
-       }
-       tagEnemy.firstChild.style.width = `${game[`${dataSet}-enemyHealth`]}%`
+  let index;
+  const posToAttack = game.gameMap.filter((el, i) => {
+    if (el[0] === posHero[0] + vertical && el[1] === posHero[1] + gorizont) {
+      index = i;
+      return true;
     }
-    if (pos === "hero") {
-       const tagHero = document.querySelector(".tileP")
-       game["heroHealth"] -= game["enemyPower"]
-       if (game["heroHealth"] <= 0) {
-          kill(index)
-          gameOver("Game Over!")
-          return
-       }
-       tagHero.firstChild.style.width = `${game["heroHealth"]}%`
+  });
+  if (posToAttack.length === 0) return;
+  const pos = posToAttack[0][2];
+  if (type === "hero" && pos === "enemy") {
+    const dataSet = posToAttack[0][3];
+    const tagEnemy = document.querySelector(`[data-enemy = '${dataSet}']`);
+    game[`${dataSet}-enemyHealth`] -= game.heroPower;
+    if (game[`${dataSet}-enemyHealth`] <= 0) {
+      kill(index);
+      if (document.querySelectorAll(".tileE").length === 0)
+        gameOver("You Won!");
+      return;
     }
- }
+    tagEnemy.firstChild.style.width = `${game[`${dataSet}-enemyHealth`]}%`;
+  }
+  if (pos === "hero") {
+    const tagHero = document.querySelector(".tileP");
+    game["heroHealth"] -= game["enemyPower"];
+    if (game["heroHealth"] <= 0) {
+      kill(index);
+      gameOver("Game Over!");
+      return;
+    }
+    tagHero.firstChild.style.width = `${game["heroHealth"]}%`;
+  }
+}
--- index.js
+++ index.js
@@ -1,155 +1,159 @@
 class Game {
-   constructor() {
-      this.container = document.querySelector('.field')
-      this.popup = document.querySelector(".popup")
-      this.enemyPower = 10
-      this.widthField = this.container.offsetWidth
-      this.heightField = this.container.offsetHeight
-      this.direction = [
-         {direct:"right",where:[1, 0]},
-         {direct:"left",where:[-1, 0]},
-         {direct:"top",where:[0, -1]},
-         {direct:"bottom",where:[0, 1]}
-      ]
-      this.columns = this.widthField / 50
-      this.rows = this.heightField / 50
-      document.querySelector(".popup__btn").addEventListener("click", () => this.init())
-      document.addEventListener("keydown", (e) => {
-         if (this.popup.style.display !== "none") return
-         const keyName = e.key
-         this.enemyRandomMove = this.direction[randomNum(this.direction.length, 0)].direct
-         if (keyName === "w" || keyName==="ц") this.move("top")
-         if (keyName === "s" || keyName==="ы") this.move("bottom")
-         if (keyName === "a" || keyName==="ф") this.move("left")
-         if (keyName === "d" || keyName==="в") this.move("right")
-         if (keyName === " ") this.attack()
-      })
-   }
-   init() {
-      this.config()
-      startGame()
-   }
-   config() {
-      this.heroPower = 50
-      this.heroHealth = 100
-      this.popup.style.display = "none"
-      this.gameMap = this.createMap(this.rows, this.columns)
-      for (let i = 1; i <= 11; i++) {
-         this[`${i}-enemyHealth`] = 100
+  constructor() {
+    this.container = document.querySelector(".field");
+    this.popup = document.querySelector(".popup");
+    this.enemyPower = 10;
+    this.widthField = this.container.offsetWidth;
+    this.heightField = this.container.offsetHeight;
+    this.direction = [
+      { direct: "right", where: [1, 0] },
+      { direct: "left", where: [-1, 0] },
+      { direct: "top", where: [0, -1] },
+      { direct: "bottom", where: [0, 1] },
+    ];
+    this.columns = this.widthField / 50;
+    this.rows = this.heightField / 50;
+    document
+      .querySelector(".popup__btn")
+      .addEventListener("click", () => this.init());
+    document.addEventListener("keydown", (e) => {
+      if (this.popup.style.display !== "none") return;
+      const keyName = e.key;
+      this.enemyRandomMove =
+        this.direction[randomNum(this.direction.length, 0)].direct;
+      if (keyName === "w" || keyName === "ц") this.move("top");
+      if (keyName === "s" || keyName === "ы") this.move("bottom");
+      if (keyName === "a" || keyName === "ф") this.move("left");
+      if (keyName === "d" || keyName === "в") this.move("right");
+      if (keyName === " ") this.attack();
+    });
+  }
+  init() {
+    this.config();
+    startGame();
+  }
+  config() {
+    this.heroPower = 50;
+    this.heroHealth = 100;
+    this.popup.style.display = "none";
+    this.gameMap = this.createMap(this.rows, this.columns);
+    for (let i = 1; i <= 11; i++) {
+      this[`${i}-enemyHealth`] = 100;
+    }
+  }
+  createMap() {
+    const array = [];
+    for (let row = 0; row < game.rows; row++) {
+      for (let column = 0; column < game.columns; column++) {
+        array.push([column, row, "wall"]);
       }
-   }
-   createMap() {
-      const array = []
-      for (let row = 0; row < game.rows; row++) {
-         for (let column = 0; column < game.columns; column++) {
-            array.push([column, row, "wall"])
-         }
-      }
-      return array
-   }
-   move(direction) {
-      if (moveHandler(direction)) {
-         fight("enemy")
-         moveHandler(this.enemyRandomMove,"enemy")
-      }
-   }
-   attack() {
-      fight()
-      fight("enemy")
-      moveHandler(this.enemyRandomMove, "enemy")
-   }
+    }
+    return array;
+  }
+  move(direction) {
+    if (moveHandler(direction)) {
+      fight("enemy");
+      moveHandler(this.enemyRandomMove, "enemy");
+    }
+  }
+  attack() {
+    fight();
+    fight("enemy");
+    moveHandler(this.enemyRandomMove, "enemy");
+  }
 }
 
 function startGame() {
-   spawn("roads")
-   spawn("rooms")
-   spawn("hero", 1)
-   spawn("enemy",10)
-   spawn("potion",10)
-   spawn("sword",2)
-   render()
+  spawn("roads");
+  spawn("rooms");
+  spawn("hero", 1);
+  spawn("enemy", 10);
+  spawn("potion", 10);
+  spawn("sword", 2);
+  render();
 }
 
-function spawn(type,count) {
-   let k = 1
-   if (type==="roads") return createRoads()
-   if (type==="rooms") return createRooms()
-   while (k<count+1) {
-      let num = game.columns * game.rows
-      let randNum = randomNum(num, 0)
-      if (game.gameMap[randNum][2]==="tile") {
-         k++
-         game.gameMap[randNum][2] = type
-         if (type === "enemy") game.gameMap[randNum][3] = k
-      }
-   }
+function spawn(type, count) {
+  let k = 1;
+  if (type === "roads") return createRoads();
+  if (type === "rooms") return createRooms();
+  while (k < count + 1) {
+    let num = game.columns * game.rows;
+    let randNum = randomNum(num, 0);
+    if (game.gameMap[randNum][2] === "tile") {
+      k++;
+      game.gameMap[randNum][2] = type;
+      if (type === "enemy") game.gameMap[randNum][3] = k;
+    }
+  }
 }
 
 function fight(persone = "hero") {
-   const pos = game.gameMap.filter((el) => el[2] === persone)
-   pos.map(el => {
-      game.direction.map(dir => {
-         fightValidate(dir.where[0], dir.where[1], el, persone)
-      })
-   })
+  const pos = game.gameMap.filter((el) => el[2] === persone);
+  pos.map((el) => {
+    game.direction.map((dir) => {
+      fightValidate(dir.where[0], dir.where[1], el, persone);
+    });
+  });
 }
 
 function render() {
-   game.container.querySelectorAll(".tile")
-      .forEach(el => el.parentNode.removeChild(el))
-   game.gameMap.map(el => {
-      const div = document.createElement("div")
-      const divHealth = document.createElement("div")
-      divHealth.classList.add("health")
-      div.classList.add("tile")
-      game.container.appendChild(div)
-      switch (el[2]) {
-         case ("wall"):
-            div.classList.add("tileW")
-            break;
-         case ("hero"):
-            div.appendChild(divHealth)
-            div.classList.add("tileP")
-            const tagHero = document.querySelector(".tileP")
-            tagHero.firstChild.style.width = `${game["heroHealth"]}%`
-            break;
-         case ("sword"):
-            div.classList.add("tileSW")
-            break;
-         case ("potion"):
-            div.classList.add("tileHP")
-            break;
-         case ("enemy"):
-            div.appendChild(divHealth)
-            div.classList.add("tileE")
-            div.dataset.enemy = `${el[3]}`
-            const tagEnemy = document.querySelector(`[data-enemy = '${el[3]}']`)
-            tagEnemy.firstChild.style.width = `${game[`${el[3]}-enemyHealth`]}%`
-            break;
-      }
-   })
+  game.container
+    .querySelectorAll(".tile")
+    .forEach((el) => el.parentNode.removeChild(el));
+  game.gameMap.map((el) => {
+    const div = document.createElement("div");
+    const divHealth = document.createElement("div");
+    divHealth.classList.add("health");
+    div.classList.add("tile");
+    game.container.appendChild(div);
+    switch (el[2]) {
+      case "wall":
+        div.classList.add("tileW");
+        break;
+      case "hero":
+        div.appendChild(divHealth);
+        div.classList.add("tileP");
+        const tagHero = document.querySelector(".tileP");
+        tagHero.firstChild.style.width = `${game["heroHealth"]}%`;
+        break;
+      case "sword":
+        div.classList.add("tileSW");
+        break;
+      case "potion":
+        div.classList.add("tileHP");
+        break;
+      case "enemy":
+        div.appendChild(divHealth);
+        div.classList.add("tileE");
+        div.dataset.enemy = `${el[3]}`;
+        const tagEnemy = document.querySelector(`[data-enemy = '${el[3]}']`);
+        tagEnemy.firstChild.style.width = `${game[`${el[3]}-enemyHealth`]}%`;
+        break;
+    }
+  });
 }
 
 function moveHandler(type, persone = "hero") {
-   const indexs = []
-   let isGood = false
-   game.direction.map(dir => {
-      if (dir.direct===type) {
-         game.col=dir.where[0]
-         game.row=dir.where[1]
-      }
-   })
-   const pos = game.gameMap.filter((el, i) => {
-      if (el[2] === persone) {
-         indexs.push(i)
-         return true
-      }
-   })
-   pos.map((el, i) => {
-      if (moveValidate(game.col, game.row, el, indexs[i], persone)) isGood = true
-   })
-   return isGood
+  const indexs = [];
+  let isGood = false;
+  game.direction.map((dir) => {
+    if (dir.direct === type) {
+      game.col = dir.where[0];
+      game.row = dir.where[1];
+    }
+  });
+  const pos = game.gameMap.filter((el, i) => {
+    if (el[2] === persone) {
+      indexs.push(i);
+      return true;
+    }
+  });
+  pos.map((el, i) => {
+    if (moveValidate(game.col, game.row, el, indexs[i], persone)) isGood = true;
+  });
+  return isGood;
 }
 
-const game = new Game()
-game.init()
+const game = new Game();
+game.init();
